27c5ed3a05d60416465bb1fa40d0b25851dad978
Artem Chepurnoy <artemchep@gmail.com>
2019-07-23 11:44:28 +0300

    Better designed list items of AirPods

--- app/src/main/java/com/artemchep/acpods/ui/adapters/AirPodsAdapter.kt
+++ app/src/main/java/com/artemchep/acpods/ui/adapters/AirPodsAdapter.kt
@@ -3,12 +3,12 @@ package com.artemchep.acpods.ui.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.core.view.isVisible
 import com.artemchep.acpods.R
-import com.artemchep.acpods.data.AirPod
 import com.artemchep.acpods.data.AirPods
 import com.artemchep.acpods.ui.interfaces.OnItemClickListener
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_airpods2.*
 
 /**
  * @author Artem Chepurnoy
@@ -24,28 +24,7 @@ class AirPodsAdapter : AdapterBase<AirPods, AirPodsAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val context = holder.itemView.context
         val airPods = this[position]
-        holder.apply {
-            addressTextView.text = airPods.info.address + ", " + airPods.info.rssi
-            stateTextView.text = listOf(
-                airPods.leftPod?.batteryLevel to R.string.airpods_left_battery,
-                airPods.rightPod?.batteryLevel to R.string.airpods_right_battery,
-                airPods.case?.batteryLevel to R.string.airpods_case_battery
-            ).mapNotNull { (batteryLevel, stringRes) ->
-                batteryLevel?.let { context.getString(stringRes, it) }
-            }.joinToString().capitalize()
-        }
-    }
-
-    private fun bindAirPod(airPod: AirPod?, imageView: ImageView, batteryTextView: TextView) {
-        if (airPod != null) {
-            batteryTextView.text = "${airPod.batteryLevel}%"
-            batteryTextView.alpha = 1.0f
-            imageView.alpha = 1.0f
-        } else {
-            batteryTextView.text = "n/a"
-            batteryTextView.alpha = 0.4f
-            imageView.alpha = 0.4f
-        }
+        holder.bind(airPods)
     }
 
     /**
@@ -54,11 +33,30 @@ class AirPodsAdapter : AdapterBase<AirPods, AirPodsAdapter.ViewHolder>() {
     class ViewHolder(
         view: View,
         listener: OnItemClickListener<Int>
-    ) : AdapterBase.ViewHolderBase(view, listener) {
+    ) : AdapterBase.ViewHolderBase(view, listener), LayoutContainer {
+        override val containerView: View?
+            get() = itemView
 
-        internal val addressTextView = view.findViewById<TextView>(R.id.addressView)
-        internal val stateTextView = view.findViewById<TextView>(R.id.stateView)
+        fun bind(airPods: AirPods) {
+            addressView.text = airPods.info.address
+            signalView.text = airPods.info.rssi.toString()
 
+            airPods.leftPod?.batteryLevel.let {
+                val isShown = it != null
+                leftPodImage.isVisible = isShown
+                leftPodBatteryView.isVisible = isShown
+
+                if (isShown) leftPodBatteryView.text = "$it %"
+            }
+
+            airPods.rightPod?.batteryLevel.let {
+                val isShown = it != null
+                rightPodImage.isVisible = isShown
+                rightPodBatteryView.isVisible = isShown
+
+                if (isShown) rightPodBatteryView.text = "$it %"
+            }
+        }
     }
 
 }
